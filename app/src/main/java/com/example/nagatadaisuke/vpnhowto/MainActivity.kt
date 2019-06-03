0c164357e01820fdecb91f1e7fffee550d70568f
daisukenagata <dbank0208@gmail.com>
2019-06-04 07:28:28 +0900

    add

--- app/src/main/java/com/example/nagatadaisuke/vpnhowto/MainActivity.kt
+++ app/src/main/java/com/example/nagatadaisuke/vpnhowto/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
